79d97451ce73f880812978e6289301ea028d7429
Mirek Rup <miroslawrup7@gmail.com>
2022-03-21 17:58:56 +0100

    Finish

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+//ACCORDION
+
 const accBoxes = document.querySelectorAll(".effects__faq-accordion");
 
 accBoxes.forEach(function (accBox) {
@@ -8,9 +10,6 @@ const accTitles = document.querySelectorAll(".accordion");
 
 accTitles.forEach(function (accTitle) {
     accTitle.addEventListener("click", function (e) {
-        // if (e.target.classList.contains("active")) {
-        // }
-
         accBoxes.forEach(function (accBox) {
             if (e.target.parentElement === accBox) {
                 accBox.classList.toggle("active");
@@ -18,7 +17,33 @@ accTitles.forEach(function (accTitle) {
                 accBox.classList.remove("active");
             }
         });
+    });
+});
 
-        // e.target.parentElement.classList.add("active");
+// MOBILE MENU
+const nav = document.querySelector(".navigation");
+const ham = document.querySelector(".hamburger");
+// const mobileClose = document.querySelector(".mobile-close");
+const dropMenu = document.querySelectorAll(".sub-nav-btn");
+
+ham.addEventListener("click", (e) => {
+    nav.classList.add("mobile-open");
+    // document.body.classList.add("no-scroll");
+});
+
+document.addEventListener("click", (e) => {
+    if (!e.target.closest(".navigation") && !e.target.closest(".hamburger")) {
+        nav.classList.remove("mobile-open");
+        // document.body.classList.remove("no-scroll");
+        dropMenu.forEach((element) => {
+            element.nextElementSibling.classList.remove("drop-down");
+        });
+    }
+});
+
+dropMenu.forEach((element) => {
+    element.addEventListener("click", (e) => {
+        e.target.nextElementSibling.classList.toggle("drop-down");
+        element.classList.toggle("drop-menu-down");
     });
 });
